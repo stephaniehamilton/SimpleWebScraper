8b2ebae06a629cac3a5158acbabf94ee23fcd401
stephaniehamilton <smhamilton0208@gmail.com>
2017-07-03 00:01:59 -0400

    Added scraper and started programming

--- Program.cs
+++ Program.cs
@@ -1,15 +1,60 @@
-﻿using System;
+﻿using SimpleWebScraper.Builders;
+using SimpleWebScraper.Data;
+using SimpleWebScraper.Workers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace SimpleWebScraper
 {
     class Program
     {
+        private const string Method = "search";
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter which city you would like to scrape information from.");
+
+            var craigsListCity = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Please enter the CraigsList category that you would like to scrape.");
+
+            var craigsListCategory = Console.ReadLine() ?? string.Empty;
+
+            using (WebClient client = new WebClient())
+            {
+                string content = client.DownloadString($"http://{craigsListCity.Replace(" ", string.Empty)}.craigslist.org/{Method}/{craigsListCategory}");
+
+                ScrapeCriteria scrapeCriteria = new ScrapeCriteriaBuilder()
+                    .WithData(content)
+                    .WithRegex(@"<a href=\""(.*?)"" data-id=\""(.?)\"" class=\""result-title hdrlnk\"">(.*?)</a>")
+                    .WithRegexOption(RegexOptions.ExplicitCapture)
+                    .WithPart(new ScrapeCriteriaPartBuilder()
+                        .WithRegex(@">(.*?)</a>")
+                        .WithRegexOption(RegexOptions.Singleline)
+                        .Build())
+                    .WithPart(new ScrapeCriteriaPartBuilder()
+                        .WithRegex(@"href=\""(.*?)\""")
+                        .WithRegexOption(RegexOptions.Singleline)
+                        .Build())
+                    .Build();
+
+                Scraper scraper = new Workers.Scraper();
+
+                var scrapedElements = scraper.Scrape(scrapeCriteria);
+
+                if (scrapedElements.Any())
+                {
+                    foreach (var scrapedElement in scrapedElements) Console.WriteLine(scrapedElement);
+                }
+                else
+                {
+                    Console.WriteLine("There were no matches for your criteria");
+                }
+            }
         }
     }
 }
